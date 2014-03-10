c68aba3c5dbc5950a77764d7109d481adf64b2e9
makumar1982 <manish.pandey035@gmail.com>
2014-03-10 20:28:10 +0530

    Second change

--- app/views/nerds/create.blade.php
+++ app/views/nerds/create.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Look! I'm CRUDding</title>
+	<title>Look! I'm CRUding</title>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 </head>
 <body>
